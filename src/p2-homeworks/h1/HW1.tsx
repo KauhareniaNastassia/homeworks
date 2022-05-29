93a012a8a674280608dad891a537086e7920382b
Your Name <you@example.com>
2022-05-29 23:58:35 +0300

    HW1 message structure

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,33 +1,27 @@
 import React from 'react'
+import Message from "./Message";
 
-const messageData = {
+/*const messageData = {
     avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
     name: 'Some Name',
     message: 'some text',
     time: '22:00',
-}
+}*/
 
 function HW1() {
+    // @ts-ignore
     return (
         <div>
             <hr/>
             homeworks 1
-
-            {/*should work (должно работать)*/}
-
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeMessage/>*/}
-            <hr/>
+            <Message
+                avatar={'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg'}
+                name={'Some Name'}
+                message={'some text'}
+                time={'22:00'}
+            />
         </div>
-    )
+    );
 }
 
 export default HW1
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
 
-function Message() {
+
+
+type MessagePropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: MessagePropsType) {
     return (
         <div>
-
+            <img src={props.avatar} />
+            <div>
+                <h2>{props.name}</h2>
+                <p>{props.message}</p>
+                <p>{props.time}</p>
+            </div>
         </div>
     )
 }
 
+
 export default Message
+
+
+
